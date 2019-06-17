c638ed495304d0e746a7c639deec570cc7c41c0c
Soe Than <soethan@gmail.com>
2019-06-17 22:24:40 +0800

    add shortcuts

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,34 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { HotKeys } from 'react-hotkeys';
 import TaskListContainer from "./task/TaskListContainer";
 import ManageTask from "./task/ManageTask";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import keyMap from '../shortcuts';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <Switch>
-        <Route exact path="/" component={TaskListContainer} />
-        <Route path="/tasks" component={TaskListContainer} />
-        <Route path="/task/:slug" component={ManageTask} />
-        <Route path="/task" component={ManageTask} />
-      </Switch>
-      <ToastContainer autoClose={3000} hideProgressBar />
-    </div>
-  );
+class App extends Component {
+  handlers = {
+    addTask: () => this.props.history.push("/task"),
+    taskList: () => this.props.history.push("/tasks")
+  };
+
+  render() {
+    console.log(this.handlers);
+    return (
+      <div className="container-fluid">
+        <HotKeys keyMap={keyMap} handlers={this.handlers} attach={window} focused>
+          <Switch>
+            <Route exact path="/" component={TaskListContainer} />
+            <Route path="/tasks" component={TaskListContainer} />
+            <Route path="/task/:slug" component={ManageTask} />
+            <Route path="/task" component={ManageTask} />
+          </Switch>
+        </HotKeys>
+        <ToastContainer autoClose={3000} hideProgressBar />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/shortcuts/index.js
@@ -0,0 +1,6 @@
+const keyMap = {
+  addTask: 'alt+a',
+  taskList: 'alt+t'
+};
+
+export default keyMap;
