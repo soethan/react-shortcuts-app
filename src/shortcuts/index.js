b985ef4dacc71117b52bc66d738f9e3ef43d5262
Soe Than <soe.than@jpmchase.com>
2019-09-16 16:45:47 +0800

    remove bindWindowsKeyDown handling

--- src/shortcuts/index.js
+++ src/shortcuts/index.js
@@ -1,5 +1,3 @@
-import { flatten, map, values } from 'lodash';
-
 const keyMap = {
   appComponent: {
     onAddTask: 'shift+a',
@@ -10,44 +8,9 @@ const keyMap = {
   }
 };
 
-const keysList = flatten(map(values(keyMap), mapping => values(mapping)));
-const lettersInKeysList = keysList.map(key => key.split('+')[1].toUpperCase());
-
 const shortcutHandlers = {};
 Object.keys(keyMap).forEach(key => {
   shortcutHandlers[key] = {};
 });
-
-bindWindowsKeyDown();
-
-function bindWindowsKeyDown() {
-  if (!window.onkeydown) {
-    window.onkeydown = e => {
-      let key;
-      let isShift;
-      if (window.event) {
-        key = window.event.keyCode;
-        isShift = !!window.event.shiftKey;
-      } else {
-        key = e.which;
-        isShift = !!e.shiftKey;
-      }
-      if (isShift) {
-        switch (key) {
-          case 16: // ignore shift key
-            break;
-          default:
-            if (lettersInKeysList.includes(String.fromCharCode(key))) {
-              if (document.activeElement) {
-                document.activeElement.blur();
-              }
-            }
-            break;
-        }
-      }
-
-    };
-  }
-}
 
 export { keyMap, shortcutHandlers };
